5f57ce38230362d8e7ace90cbdff358b85a40f60
duduardo45 <eduardoeugeniosouza@gmail.com>
2025-09-02 15:12:18 -0300

    faz print bonitinho e ajusta tarefa

--- lab2/ex1.c
+++ lab2/ex1.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/shm.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,6 +11,7 @@ void printa_matriz(int* matriz, int linhas, int colunas);
 int main(int argc, char* argv[])
 {
     int mypid, pid, status, q=0;
+
     mypid = getpid();
 
     if (argc != 3)
@@ -17,20 +19,20 @@ int main(int argc, char* argv[])
         printf("Uso: %s <colunas> <linhas>\n", argv[0]);
         exit(1);
     }
-    printf("OI %d", ++q);
+
     int colunas = atoi(argv[1]);
     int linhas = atoi(argv[2]);
-    printf("OI %d", ++q);
+
     int qtd_nums = colunas*linhas;
 
-    int shmid1 = shmget(IPC_PRIVATE, qtd_nums*sizeof(int), IPC_CREAT);
-    int shmid2 = shmget(IPC_PRIVATE, qtd_nums*sizeof(int), IPC_CREAT);
-    int shmid3 = shmget(IPC_PRIVATE, qtd_nums*sizeof(int), IPC_CREAT);
-    printf("OI %d", ++q);
+    int shmid1 = shmget(IPC_PRIVATE, qtd_nums*sizeof(int), IPC_CREAT | S_IRUSR | S_IWUSR | S_IXUSR);
+    int shmid2 = shmget(IPC_PRIVATE, qtd_nums*sizeof(int), IPC_CREAT | S_IRUSR | S_IWUSR | S_IXUSR);
+    int shmid3 = shmget(IPC_PRIVATE, qtd_nums*sizeof(int), IPC_CREAT | S_IRUSR | S_IWUSR | S_IXUSR);
+
     int* matriz1 = (int*)shmat(shmid1, 0, 0);
     int* matriz2 = (int*)shmat(shmid2, 0, 0);
     int* matriz3 = (int*)shmat(shmid3, 0, 0);
-    printf("OI %d", ++q);
+
     for (int i = 0; i < 2*qtd_nums; i++) {
         if (i%2) {
             matriz2[i/2] = i;
@@ -38,10 +40,14 @@ int main(int argc, char* argv[])
             matriz1[i/2] = i;
         }
     }
-    printf("OI %d", ++q);
+
     for (int i = 0; i<linhas; i++) {
         pid = fork();
-        if (pid) {
+        if (pid < 0) {
+            printf("Erro no fork\n");
+            exit(1);
+        }
+        if (pid == 0) {
             for (int j = 0; j<colunas; j++) {
                 int idx = i*colunas + j;
                 matriz3[idx] = matriz1[idx] + matriz2[idx];
@@ -52,12 +58,12 @@ int main(int argc, char* argv[])
             exit(0);
         }
     }
-    printf("OI %d", ++q);
+
     for (int i = 0; i<linhas; i++) wait(&status);
-    printf("OI %d", ++q);
+
     printa_matriz(matriz1, linhas, colunas);
     printa_matriz(matriz2, linhas, colunas);
-    printa_matriz(matriz2, linhas, colunas);
+    printa_matriz(matriz3, linhas, colunas);
 
     shmdt(matriz1);
     shmdt(matriz2);
@@ -72,11 +78,20 @@ int main(int argc, char* argv[])
 
 void printa_matriz(int* matriz, int linhas, int colunas) {
     printf("Matriz:\n");
+    for (int j = 0; j<colunas; j++) {
+        printf("--------");
+    }
+    printf("--------\n");
     for (int i = 0; i<linhas; i++) {
+        printf("|\t");
         for (int j = 0; j<colunas; j++) {
             int idx = i*colunas + j;
-            printf("%d ", matriz[idx]);
+            printf("%d\t", matriz[idx]);
         }
-        printf("\n");
+        printf("|\n");
     }
+    for (int j = 0; j<colunas; j++) {
+        printf("--------");
+    }
+    printf("--------\n");
 }
