a6952721a7b1abb48eb54d0964d16add7b3db2e6
Luiz Felipe Neves <luizfneves@proton.me>
2025-09-11 14:52:27 -0300

    ate o 4

--- lab3/exc2.c
+++ lab3/exc2.c
@@ -3,21 +3,21 @@
 #include <stdlib.h>
 
 void intHandler(int sinal);
-void quitHandler(int sinal);
+void killHandler(int sinal);
 
 int main(void) {
   void (*p)(int); // ponteiro para função que recebe int como
   // parâmetro
   p = signal(SIGINT, intHandler);
   printf("Endereco do manipulador anterior %p\n", p);
-  p = signal(SIGQUIT, quitHandler);
+  p = signal(SIGKILL, killHandler);
   printf("Endereco do manipulador anterior %p\n", p);
   puts("Ctrl-C desabilitado. Use Ctrl-\\ para terminar");
   for (;;)
     ;
 }
 void intHandler(int sinal) { printf("Você pressionou Ctrl-C (%d) \n", sinal); }
-void quitHandler(int sinal) {
-  printf("Terminando o processo...\n");
+void killHandler(int sinal) {
+  printf("Matando o processo...\n");
   exit(0);
 }
---
+++ lab3/exc4.c
@@ -0,0 +1,41 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  pid_t pid;
+
+  pid_t pids[2] = {-1, -1};
+
+  for (int i = 0; i < 2; i++) {
+    if ((pid = fork()) < 0) {
+      fprintf(stderr, "Erro ao criar filho\n");
+      exit(-1);
+    }
+    if (pid == 0) { /* child */
+      for (;;) {
+        ;
+      }
+    }
+    kill(pid, SIGSTOP);
+    pids[i] = pid;
+  }
+
+  kill(pids[0], SIGSTOP);
+
+  for (int i = 0; i < 5; i++) {
+    kill(pids[0], SIGSTOP);
+    kill(pids[1], SIGCONT);
+    usleep(10000);
+    kill(pids[1], SIGSTOP);
+    kill(pids[0], SIGCONT);
+    usleep(10000);
+  }
+
+  kill(pids[0], SIGKILL);
+  kill(pids[1], SIGKILL);
+
+  return 0;
+}
